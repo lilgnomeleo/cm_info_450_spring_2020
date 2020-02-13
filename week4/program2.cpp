1e5050ed86fcebdd0aed796e7aa7ad7b318e3460
lilgnomeleo <carleomitolo@gmail.com>
2020-02-13 21:16:42 +0000

    updated program 2 and moved the .replit by mistake

--- week4/program2.cpp
+++ week4/program2.cpp
@@ -1,22 +1,24 @@
 #include <iostream>
 #include <stdio.h>
-#include <cstring>
 
 using namespace std;
 
 int main(){
-    string students[100];
-    char nextStudent[100];
-    int numstudents = 0;
-    do{
-        cout << "Enter the student at index " << numstudents << ": ";
-        cin.getline(nextStudent,100);
-        students[numstudents] = nextStudent;
-        numstudents++;
-    }
-    while(strlen(nextStudent)>1);
+    // define
+    string name[30];
+    char nxtName[100];
+    int num = 0;
 
-    for(int i = 0; i < numstudents-1; i++){
-        cout << "Student at index [" << i << "]: " << students[i] << "\n";
+    //do lo
+    do{
+        cout << "Enter the student at index " << num << ": ";
+        cin.getline(nxtName,100);
+        name[num] = nxtName;
+        num++;
+    }
+    while(nxtName[0]);
+
+    for(int i = 0; i < num-1; i++){
+        cout << "Student at index [" << i << "]: " << name[i] << endl;
     }
 }
